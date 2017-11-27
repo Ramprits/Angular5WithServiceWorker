a8416bca2ad34adcd6b70ab73d9499ba7ba6cfa4
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-11-27 13:59:44 +0530

    customer added

--- src/app/department/department.module.ts
+++ src/app/department/department.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SoftwareDeveloperComponent } from './software-developer/software-developer.component';
 import { HumanResourceComponent } from './human-resource/human-resource.component';
 import { WebDeveloperComponent } from './web-developer/web-developer.component';
+import { CustomerComponent } from './customer/customer.component';
+import { RouterModule } from '@angular/router';
+import { departmentRoutes } from './department.routing';
+import { PanelModule, SplitButtonModule, DataTableModule, ButtonModule } from 'primeng/primeng';
+import { CustomerService } from './customer/customer.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, HttpClientModule, RouterModule.forChild(departmentRoutes), SplitButtonModule, PanelModule, DataTableModule, ButtonModule
   ],
-  declarations: [SoftwareDeveloperComponent, HumanResourceComponent, WebDeveloperComponent]
+  declarations: [SoftwareDeveloperComponent, HumanResourceComponent, WebDeveloperComponent, CustomerComponent],
+  providers: [CustomerService]
 })
 export class DepartmentModule { }
---
+++ src/app/department/department.routing.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { CustomerComponent } from './customer/customer.component';
+
+export const departmentRoutes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'customer' },
+    { path: 'customer', component: CustomerComponent },
+];
